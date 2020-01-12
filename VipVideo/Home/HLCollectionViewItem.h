bb3d4882eac1cb66754572926226316413448fd1
iodefog <iodefog@meituan.com>
2020-01-12 21:15:17 +0800

    CollectionView

---
+++ VipVideo/Home/HLCollectionViewItem.h
@@ -0,0 +1,18 @@
+//
+//  HLCollectionViewItem.h
+//  VipVideo
+//
+//  Created by 李红力 on 2020/1/10.
+//  Copyright © 2020 SV. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HLCollectionViewItem : NSCollectionViewItem
+@property (weak) IBOutlet NSTextField *textLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
