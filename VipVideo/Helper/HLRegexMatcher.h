83b2c7c2d76591ea4ab48454cc0b50f2104800d4
iodefog <iodefog@meituan.com>
2023-01-17 18:54:33 +0800

    1. 修复腾讯视频不能使用VIP问题</br> 2.替换部分失效导航网站 </br> 3. pkg支持Mac M1安装

---
+++ VipVideo/Helper/HLRegexMatcher.h
@@ -0,0 +1,20 @@
+//
+//  HLRegexMatcher.h
+//  VipVideo
+//
+//  Created by 李红力 on 2023/1/17.
+//  Copyright © 2023 SV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HLRegexMatcher : NSObject
+
++ (BOOL)isValidVideoUrl:(NSString *)url;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
